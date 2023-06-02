7ddc49f3f1f04c0380a74cc1ec537fd19834e783
Aryansharma9917 <sharmaaryan9837@gmail.com>
2023-06-02 16:48:59 +0530

    Function to find one user

--- db/function.go
+++ db/function.go
@@ -1,10 +1,24 @@
 package db
 
 import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Used to find one user
 func FindOne(username string, db string, collection string, client *mongo.Client) (*User, error) {
-
+	var user User
+	coll := client.Database(db).Collection(collection)
+	err := coll.FindOne(context.Background(), bson.M{"username": username}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
+
+// Used to delete one user
