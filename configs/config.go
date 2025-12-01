0920d2cce592ab49458132b8374882a1186a1745
Aryan Sharma <sharmaaryan9837@gmail.com>
2025-12-01 16:25:49 +0000

    Updated configs file

--- configs/config.go
+++ configs/config.go
@@ -3,11 +3,11 @@ package configs
 type ConfigDatabase struct {
 	Port                string `yaml:"port" env:"PORT" env-default:"8080"`
 	Host                string `yaml:"host" env:"HOST" env-default:"localhost"`
-	DatabaseName        string `yaml:"host" env:"DBNAME" env-default:"goapi-auth"`
-	UserCollection      string `yaml:"host" env:"USERCLC" env-default:"users"`
-	TokenCollection     string `yaml:"host" env:"TOKENS" env-default:"TOKENS"`
-	BlacklistCollection string `yaml:"host" env:"BLACKLISTCLC" env-default:"blacklisted-tokens"`
-	JwtSecret           string `yaml:"host" env:"JWTSECRET"  env-default:"aryansharma"`
+	DatabaseName        string `yaml:"dbname" env:"DBNAME" env-default:"goapi-auth"`
+	UserCollection      string `yaml:"userCollection" env:"USERCLC" env-default:"users"`
+	TokenCollection     string `yaml:"tokenCollection" env:"TOKENS" env-default:"tokens"`
+	BlacklistCollection string `yaml:"blacklistCollection" env:"BLACKLISTCLC" env-default:"blacklisted-tokens"`
+	JwtSecret           string `yaml:"jwtSecret" env:"JWTSECRET" env-default:"supersecretkey123"`
 }
 
 var Cfg ConfigDatabase
