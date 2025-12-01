03ca4b16005e69ca54b2676016829ac8a146fb97
Aryan Sharma <sharmaaryan9837@gmail.com>
2025-12-01 16:26:31 +0000

    Updated databace connection

--- db/mongo.go
+++ db/mongo.go
@@ -10,22 +10,26 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Used to connect to a databasea
+// Used to connect to a database
 func ConnectDB() (*mongo.Client, error) {
-	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
-
 	uri := "mongodb://localhost:27017"
-	clientOptions := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPIOptions)
+
+	clientOptions := options.Client().ApplyURI(uri)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		return nil, err
 	}
+
+	// Ping the database to verify connection
 	var result bson.M
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+	if err := client.Database("admin").RunCommand(ctx, bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
 		return nil, err
 	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	fmt.Println("Connected to MongoDB!")
 	return client, nil
 }
